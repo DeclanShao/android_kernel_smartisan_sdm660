68d5a8878ebec02b0c1291c8786c2d45ca193c3e
DeclanShao <2378677167@qq.com>
2025-01-07 17:23:20 +0000

    audio: smartisan: Update changes

--- techpack/audio/dsp/q6afe.c
+++ techpack/audio/dsp/q6afe.c
@@ -919,9 +919,9 @@ static int32_t sp_make_afe_callback(uint32_t opcode, uint32_t *payload,
 	}
 
 #ifdef CONFIG_MACH_SMARTISAN_SDM660
-	if (param_id == AFE_PARAM_ID_DSM_CFG  ||
-		param_id == AFE_PARAM_ID_DSM_INFO ||
-		param_id == AFE_PARAM_ID_CALIB) {
+	if (param_hdr.param_id == AFE_PARAM_ID_DSM_CFG  ||
+		param_hdr.param_id == AFE_PARAM_ID_DSM_INFO ||
+		param_hdr.param_id == AFE_PARAM_ID_CALIB) {
 		struct afe_dsm_get_resp *dsm_resp =
 			(struct afe_dsm_get_resp *) payload;
 		if (payload_size < sizeof(*dsm_resp)) {
